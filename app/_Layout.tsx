193f6bca05a04eae2fcfeb65542eeb1fe0ecf65a
nadamohsen139 <mohsennada139@gmail.com>
2025-01-27 22:08:24 +0200

    first commit

---
+++ app/_Layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import "../global.css";
+
+const _Layout = () => {
+  return (
+    <View>
+      <Text className="text-red-500">_Layout</Text>
+    </View>
+  );
+};
+
+export default _Layout;
